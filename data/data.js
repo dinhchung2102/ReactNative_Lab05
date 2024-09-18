df235ff96a5d913c6c8c0ec17e0a2976322400dd
dinhchung2102 <vodinhchung4@gmail.com>
2024-09-18 09:39:01 +0700

    upload a sample data

---
+++ data/data.js
@@ -0,0 +1,34 @@
+export const dataMobile = [
+    {
+        id : '001',
+        name:'Dien thoai VSmart Joy 3 - Hang chinh hang',
+        image: require('../assets/vs_blue.png'),
+        color: 'blue',
+        price: 1790000,
+        supplier: 'Tiki Trading',
+    },
+    {
+        id: '002',
+        name:'Dien thoai VSmart Joy 3 - Hang chinh hang',
+        image: require('../assets/vs_red.png'),
+        color: 'red',
+        price: 1790000,
+        supplier:'Tiki Trading'
+    },
+    {
+        id: '003',
+        name:'Dien thoai VSmart Joy 3 - Hang chinh hang',
+        image: require('../assets/vs_silver.png'),
+        color: 'silver',
+        price: 1790000,
+        supplier:'Tiki Trading'
+    },
+    {
+        id: '004',
+        name:'Dien thoai VSmart Joy 3 - Hang chinh hang',
+        image: require('../assets/vs_black.png'),
+        color: 'black',
+        price: 1790000,
+        supplier:'Tiki Trading'
+    }
+]
