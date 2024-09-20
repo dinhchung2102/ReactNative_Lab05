072f6ab63dbe88ad46f4ae105100bc3abad1d36c
dinhchung2102 <vodinhchung4@gmail.com>
2024-09-20 21:51:19 +0700

    passing parameters to routes

--- App.js
+++ App.js
@@ -6,8 +6,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Screen_01 from './screens/Screen_01'
 import Screen_02 from './screens/Screen_02';
 import Screen_03 from './screens/Screen_03';
-import Screen_04 from './screens/Screen_04';
-import Screen_05 from './screens/Screen_05';
 import Screen_Buy from './screens/Screen_Buy';
 
 export default function App() {
@@ -18,8 +16,7 @@ export default function App() {
         <Stack.Screen name='Screen_01' component={Screen_01}/>
         <Stack.Screen name='Screen_02' component={Screen_02}/>
         <Stack.Screen name='Screen_03' component={Screen_03}/>
-        <Stack.Screen name='Screen_04' component={Screen_04}/>
-        <Stack.Screen name='Screen_05' component={Screen_05}/>
+        
         <Stack.Screen name='Screen_Buy' component={Screen_Buy}/>
       </Stack.Navigator>
     </NavigationContainer>
--- screens/Screen_02.js
+++ screens/Screen_02.js
@@ -1,100 +1,175 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
-const Screen_02 = ({ navigation }) => {
-    return (
-        <View style={styles.container}>
-            
-            <View style={styles.header}>
-                <Image
-                    source={require('../assets/vs_blue.png')}
-                    style={styles.imageStyle}
-                />
-                
-                <View style={styles.textView}>
-                    <Text style={styles.text}>Điện thoại VsMart Joy 3</Text>
-                    <Text style={styles.text}>Hàng Chính Hãng</Text>
-                </View>
-            </View>
-            
-            <View style={styles.content}>
-                <Text style={styles.text}>Chọn một màu bên dưới:</Text>
-                
-                <View style={styles.pickColor}>
-                    <TouchableOpacity onPress={() => {navigation.navigate('Screen_03')}}>
-                        <View style={{ backgroundColor: '#C5F1FB', height: 100, width: 100, marginBottom: 30, marginTop: 10 }} />
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity onPress={() => {navigation.navigate('Screen_04')}}>
-                        <View style={{ backgroundColor: '#F30D0D', height: 100, width: 100, marginBottom: 30 }} />
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity onPress={() => {navigation.navigate('Screen_05')}}>
-                        <View style={{ backgroundColor: 'black', height: 100, width: 100, marginBottom: 30 }} />
-                    </TouchableOpacity>
-                    
-                    <TouchableOpacity onPress={() => {navigation.navigate('Screen_01')}}>
-                        <View style={{ backgroundColor: '#234896', height: 100, width: 100, marginBottom: 10 }} />
-                    </TouchableOpacity>
-                </View>
-                
-                <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>XONG</Text>
-                </TouchableOpacity>
-            </View>
+const Screen_02 = ({ route, navigation }) => {
+  const { itemId, name, image, colorPro, supplier, price } = route.params;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={image} style={styles.imageStyle} />
+
+        <View style={styles.textView}>
+          <Text style={styles.text}>{name}</Text>
+          <Text style={styles.text}>Màu: {colorPro}</Text>
+          <Text style={styles.text}>Cung cấp bởi: {supplier}</Text>
+          <Text style={styles.text}>Giá: {price}đ</Text>
         </View>
-    );
+      </View>
+
+      <View style={styles.content}>
+        <Text style={styles.text}>Chọn một màu bên dưới:</Text>
+
+        <View style={styles.pickColor}>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Screen_03",{
+                itemId: '002',
+                name: 'Điện Thoại Vsmart Joy 3 Hàng chính hãng',
+                image: require('../assets/vs_silver.png'),
+                colorPro: 'Bạc',
+                supplier: 'Tiki Trading',
+                price: 1790000,
+              });
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "#C5F1FB",
+                height: 100,
+                width: 100,
+                marginBottom: 30,
+                marginTop: 10,
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Screen_03", {
+                itemId: '003',
+                name: 'Điện Thoại Vsmart Joy 3 Hàng chính hãng',
+                image: require('../assets/vs_red.png'),
+                colorPro: 'Đỏ',
+                supplier: 'Tiki Trading',
+                price: 1790000,
+              });
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "#F30D0D",
+                height: 100,
+                width: 100,
+                marginBottom: 30,
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Screen_03", {
+                itemId: '004',
+                name: 'Điện Thoại Vsmart Joy 3 Hàng chính hãng',
+                image: require('../assets/vs_black.png'),
+                colorPro: 'Đen',
+                supplier: 'Tiki Trading',
+                price: 1790000,
+              });
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "black",
+                height: 100,
+                width: 100,
+                marginBottom: 30,
+              }}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("Screen_03", {
+                itemId: "001",
+                name: "Điện Thoại Vsmart Joy 3 Hàng chính hãng",
+                image: require("../assets/vs_blue.png"),
+                colorPro: "Xanh",
+                supplier: "Tiki Trading",
+                price: 1790000,
+              });
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "#234896",
+                height: 100,
+                width: 100,
+                marginBottom: 10,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>XONG</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        // backgroundColor: 'red'
-    },
-    header: {
-        flex: 1,
-        // backgroundColor: 'green',
-        flexDirection: 'row'
-    },
-    content: {
-        flex: 3,
-        backgroundColor: '#C4C4C4'
-    },
-    imageStyle: {
-        marginLeft: '5%',
-        marginTop: '10%',
-        height: 120,
-        width: 100
-    },
-    textView: {
-        flexDirection: 'column',
-        marginTop: '10%',
-        marginLeft: 15
-    },
-    text: {
-        color: 'black',
-        fontSize: 18
-    },
-    pickColor: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10
-    },
-    button: {
-        width: '90%',
-        backgroundColor: 'blue',
-        marginLeft: '5%',
-        borderRadius: 10,
-        height: '8%',
-        marginTop: '10%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText: {
-        fontSize: 30,
-        fontWeight: 'bold',
-        color: 'white'
-    }
+  container: {
+    flex: 1,
+    // backgroundColor: 'red'
+  },
+  header: {
+    flex: 1,
+    // backgroundColor: 'green',
+    flexDirection: "row",
+  },
+  content: {
+    flex: 3,
+    backgroundColor: "#C4C4C4",
+  },
+  imageStyle: {
+    marginLeft: "2%",
+    marginTop: "10%",
+    height: 120 * 1.4,
+    width: 100 * 1.4,
+  },
+  textView: {
+    flexDirection: "column",
+    marginTop: "10%",
+    marginLeft: 15,
+    //justifyContent:'space-evenly'
+  },
+  text: {
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+  pickColor: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+  },
+  button: {
+    width: "90%",
+    backgroundColor: "blue",
+    marginLeft: "5%",
+    borderRadius: 10,
+    height: "8%",
+    marginTop: "10%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "white",
+  },
 });
 
 export default Screen_02;
